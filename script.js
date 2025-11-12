e4f92088a9b61c7a9bebcecb64712647fad3a9c0
jessi-dev-code <jaspreet.bhagat@ensuesoft.com>
2025-11-12 18:21:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,260 @@
+window.onload = function () {
+  let arr = [
+    {
+image_url: "images/1 (1).png ",
+      title: "100 Js Projects by jessi",
+      git_link:
+        "https://github.com/jessi-17/JavaScript_Projects/tree/main/100%20Js%20Projects%20by%20jessi",
+      live_demo:
+        "https://jessi-17.github.io/JavaScript_Projects/100%20Js%20Projects%20by%20jessi/",
+    },
+    {
+      image_url: "images/1 (2).png ",
+      title: "Blob Animation",
+      git_link:
+        "https://github.com/jessi-17/JavaScript_Projects/tree/main/Blob%20Animation",
+      live_demo:
+        "https://jessi-17.github.io/JavaScript_Projects/Blob%20Animation/",
+    },
+    {
+      image_url: "images/cell-1.png",
+      title: "Calculator",
+      git_link:
+        "https://github.com/jessi-17/JavaScript_Projects/tree/main/Calculator",
+      live_demo: "https://jessi-17.github.io/JavaScript_Projects/Calculator/",
+    },
+    {
+      image_url: "images/1 (10).png ",
+      title: "DigitalClock",
+      git_link:
+        "https://github.com/jessi-17/JavaScript_Projects/tree/main/DigitalClock",
+      live_demo: "https://jessi-17.github.io/JavaScript_Projects/DigitalClock/",
+    },
+    {
+      image_url: "images/1 (11).png ",
+      title: "Drum Kit with Keyboard",
+      git_link:
+        "https://github.com/jessi-17/JavaScript_Projects/tree/main/Drum%20Kit%20with%20Keyboard",
+      live_demo:
+        "https://jessi-17.github.io/JavaScript_Projects/Drum%20Kit%20with%20Keyboard/",
+    },
+    {
+      image_url: "images/1 (12).png ",
+      title: "Emoji Rating",
+      git_link:
+        "https://github.com/jessi-17/JavaScript_Projects/tree/main/Emoji%20Rating",
+      live_demo:
+        "https://jessi-17.github.io/JavaScript_Projects/Emoji%20Rating/",
+    },
+    {
+      image_url: "images/1 (7).png ",
+      title: "Expandable Gallery",
+      git_link:
+        "https://github.com/jessi-17/JavaScript_Projects/tree/main/Expandable%20Gallery",
+      live_demo:
+        "https://jessi-17.github.io/JavaScript_Projects/Expandable%20Gallery/",
+    },
+    {
+      image_url: "images/1 (14).png ",
+      title: "Gradient Buttons MI",
+      git_link:
+        "https://github.com/jessi-17/JavaScript_Projects/tree/main/Gradient%20Buttons%20MI",
+      live_demo:
+        "https://jessi-17.github.io/JavaScript_Projects/Gradient%20Buttons%20MI/",
+    },
+    {
+      image_url: "images/1 (15).png ",
+      title: "Guess A number",
+      git_link:
+        "https://github.com/jessi-17/JavaScript_Projects/tree/main/Guess%20A%20number",
+      live_demo:
+        "https://jessi-17.github.io/JavaScript_Projects/Guess%20A%20number/",
+    },
+    {
+      image_url: "images/1 (16).png ",
+      title: "Hamburger Animation",
+      git_link:
+        "https://github.com/jessi-17/JavaScript_Projects/tree/main/Hamburger%20Animation",
+      live_demo:
+        "https://jessi-17.github.io/JavaScript_Projects/Hamburger%20Animation/",
+    },
+    {
+      image_url: "images/1 (17).png ",
+      title: "Movie Slider",
+      git_link:
+        "https://github.com/jessi-17/JavaScript_Projects/tree/main/Movie%20Slider",
+      live_demo:
+        "https://jessi-17.github.io/JavaScript_Projects/Movie%20Slider/",
+    },
+    {
+      image_url: "images/1 (18).png ",
+      title: "Movie box",
+      git_link:
+        "https://github.com/jessi-17/JavaScript_Projects/tree/main/Movie%20box",
+      live_demo: "https://jessi-17.github.io/JavaScript_Projects/Movie%20box/",
+    },
+    {
+      image_url: "images/1 (19).png ",
+      title: "Oddly Satisfying Loader",
+      git_link:
+        "https://github.com/jessi-17/JavaScript_Projects/tree/main/Oddly%20Satisfying%20Loader",
+      live_demo:
+        "https://jessi-17.github.io/JavaScript_Projects/Oddly%20Satisfying%20Loader/",
+    },
+    {
+      image_url: "images/1 (20).png ",
+      title: "Pure CSS_ Cute 3D Button",
+      git_link:
+        "https://github.com/jessi-17/JavaScript_Projects/tree/main/Pure%20CSS_%20Cute%203D%20Button",
+      live_demo:
+        "https://jessi-17.github.io/JavaScript_Projects/Pure%20CSS_%20Cute%203D%20Button/",
+    },
+    {
+      image_url: "images/1 (21).png ",
+      title: "Rainbow Gradient Glowing Effect",
+      git_link:
+        "https://github.com/jessi-17/JavaScript_Projects/tree/main/Rainbow%20Gradient%20Glowing%20Effect",
+      live_demo:
+        "https://jessi-17.github.io/JavaScript_Projects/Rainbow%20Gradient%20Glowing%20Effect/",
+    },
+    {
+      image_url: "images/1 (22).png ",
+      title: "Simon Game",
+      git_link:
+        "https://github.com/jessi-17/JavaScript_Projects/tree/main/Simon%20Game",
+      live_demo: "https://jessi-17.github.io/JavaScript_Projects/Simon%20Game/",
+    },
+    {
+      image_url: "images/1 (23).png ",
+      title: "Sliding Arrow On Button Hover Effect",
+      git_link:
+        "https://github.com/jessi-17/JavaScript_Projects/tree/main/Sliding%20Arrow%20On%20Button%20Hover%20Effect",
+      live_demo:
+        "https://jessi-17.github.io/JavaScript_Projects/Sliding%20Arrow%20On%20Button%20Hover%20Effect/",
+    },
+    {
+     image_url: "images/1 (24).png ",
+      title: "Spinner Orbits",
+      git_link:
+        "https://github.com/jessi-17/JavaScript_Projects/tree/main/Spinner%20Orbits",
+      live_demo:
+        "https://jessi-17.github.io/JavaScript_Projects/Spinner%20Orbits/",
+    },
+    {
+      image_url: "images/1 (25).png ",
+      title: "Spinning Squares",
+      git_link:
+        "https://github.com/jessi-17/JavaScript_Projects/tree/main/Spinning%20Squares",
+      live_demo:
+        "https://jessi-17.github.io/JavaScript_Projects/Spinning%20Squares/",
+    },
+    {
+     image_url: "images/1 (26).png ",
+      title: "Star Rating Interaction",
+      git_link:
+        "https://github.com/jessi-17/JavaScript_Projects/tree/main/Star%20Rating%20Interaction",
+      live_demo:
+        "https://jessi-17.github.io/JavaScript_Projects/Star%20Rating%20Interaction/",
+    },
+    {
+      image_url: "images/1 (27).png ",
+      title: "Typing Animation",
+      git_link:
+        "https://github.com/jessi-17/JavaScript_Projects/tree/main/Typing%20Animation",
+      live_demo:
+        "https://jessi-17.github.io/JavaScript_Projects/Typing%20Animation/",
+    },
+    {
+      image_url: "images/1 (28).png ",
+      title: "Waves Loader",
+      git_link:
+        "https://github.com/jessi-17/JavaScript_Projects/tree/main/Waves%20Loader",
+      live_demo:
+        "https://jessi-17.github.io/JavaScript_Projects/Waves%20Loader/",
+    },
+    {
+      image_url: "images/1 (29).png ",
+      title: "Weather App",
+      git_link:
+        "https://github.com/jessi-17/JavaScript_Projects/tree/main/Weather-App",
+      live_demo: "https://jessi-17.github.io/JavaScript_Projects/Weather-App/",
+    },
+    {
+      image_url: "images/1 (30).png ",
+      title: "Glassmorphism Card",
+      git_link:
+        "https://github.com/jessi-17/JavaScript_Projects/tree/main/glassmorphism%20card",
+      live_demo:
+        "https://jessi-17.github.io/JavaScript_Projects/glassmorphism%20card/",
+    },
+    {
+      image_url: "images/cell-1.png",
+      title: "Netflix Search Option",
+      git_link:
+        "https://github.com/jessi-17/JavaScript_Projects/tree/main/netflix_search_option",
+      live_demo:
+        "https://jessi-17.github.io/JavaScript_Projects/netflix_search_option/",
+    },
+    {
+     image_url: "images/1 (31).png ",
+      title: "Perspective Change",
+      git_link:
+        "https://github.com/jessi-17/JavaScript_Projects/tree/main/perspective_change",
+      live_demo:
+        "https://jessi-17.github.io/JavaScript_Projects/perspective_change/",
+    },
+    {
+      image_url: "images/1 (32).png ",
+      title: "Personal Portfolio 1",
+      git_link:
+        "https://github.com/jessi-17/JavaScript_Projects/tree/main/portfolios/Personal_Portfolio1",
+      live_demo:
+        "https://jessi-17.github.io/JavaScript_Projects/portfolios/Personal_Portfolio1/",
+    },
+    {
+      image_url: "images/1 (33).png ",
+      title: "Rock Paper Scissors",
+      git_link:
+        "https://github.com/jessi-17/JavaScript_Projects/tree/main/rockpaperscissors",
+      live_demo:
+        "https://jessi-17.github.io/JavaScript_Projects/rockpaperscissors/",
+    },
+    {
+      image_url: "images/1 (34).png ",
+      title: "Socials",
+      git_link:
+        "https://github.com/jessi-17/JavaScript_Projects/tree/main/socials",
+      live_demo: "https://jessi-17.github.io/JavaScript_Projects/socials/",
+    },
+    {
+      image_url: "images/1 (35).png ",
+      title: "To-do List",
+      git_link:
+        "https://github.com/jessi-17/JavaScript_Projects/tree/main/todo%20list",
+      live_demo: "https://jessi-17.github.io/JavaScript_Projects/todo%20list/",
+    },
+  ];
+
+  let card_container = document.getElementById("cardBox");
+  arr.forEach(function (e) {
+    card_container.innerHTML += `
+        <div class="card">
+            <div class="image">
+                <img src="${e.image_url}" alt="">
+            </div>
+            <div class="text">
+                <h3>${e.title}</h3>
+                <div class="buttons">
+                    <a href="${e.git_link}">
+                        <button>Gitlink</button>
+                    </a>
+                    <a href="${e.live_demo}">
+                        <button>livedemo</button>
+                    </a>
+                </div>
+            </div>
+        </div>
+        
+        `;
+  });
+};
